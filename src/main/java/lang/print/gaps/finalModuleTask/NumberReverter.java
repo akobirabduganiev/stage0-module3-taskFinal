97b5a2b13603308b962f1fd3ca72043ca6ba6839
Akobir Abduganiev <akobir.abduganiev@ya.ru>
2022-10-16 18:25:06 +0500

    success

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        while (number != 0) {
+            System.out.print(number % 10);
+            number /= 10;
+        }
+
     }
 }
